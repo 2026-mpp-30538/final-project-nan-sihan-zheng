9c07019149ba6bd76997fd9831ec1190f01c8e9a
hazellou <hazel02@uchicago.edu>
2026-03-01 14:03:09 -0600

    preprocessing

--- code/processing.py
+++ code/processing.py
@@ -2,11 +2,12 @@ import geopandas as gpd
 import pandas as pd
 import os
 from shapely import wkt
+from pathlib import Path
 
-os.getcwd()
-PATH_RAW = '/Users/zackzzz/python_II_ps/final-project-nan-sihan-zheng/data/raw-data'
+script_dir = Path(__file__).parent
+raw_call = script_dir / '../data/raw-data/Call_Data_20260227.csv'
 
-call = pd.read_csv(os.path.join(PATH_RAW,'call.csv'))
+call = pd.read_csv(raw_call)
 call.head(5)
 call.shape
 
@@ -40,11 +41,26 @@ assert call_care['CAD Event Number'].duplicated().sum() == 0, 'There is duplicat
 initial_call_type = call_care['Initial Call Type'].unique()
 final_call_type = call_care['Final Call Type'].unique()
 
-call_filter = call_care[
+call_clean = call_care[
     ~(
         call['Initial Call Type'].str.contains('TEST') |
         call['Final Call Type'].str.contains('TEST') 
     )
 ].copy()
 
-call_filter.shape
+# Parse datetime + add time features (for heatmap)
+TIME_COL = 'CAD Event Original Time Queued'
+call_clean[TIME_COL] = pd.to_datetime(call_clean[TIME_COL], errors="coerce")
+call_clean = call_clean.dropna(subset=[TIME_COL]).copy()
+call_clean = call_clean.rename(columns={TIME_COL: "datetime"})
+call_clean["hour"] = call_clean["datetime"].dt.hour
+call_clean["dayofweek"] = call_clean["datetime"].dt.day_name()
+call_clean["date"] = call_clean["datetime"].dt.date
+
+# Clean lat/lon + build GeoDataFrame (for maps)
+redacted_lat = (call["Dispatch Latitude"] == "REDACTED").sum()
+redacted_lon = (call["Dispatch Longitude"] == "REDACTED").sum()
+print("Latitude redacted count:", redacted_lat)
+print("Longitude redacted count:", redacted_lon)
+
+call_clean.shape
