184e5691287f751e228850965e796204a94808fa
Zack11cz <dreamingczz7@gmail.com>
2026-03-02 14:34:58 -0600

    Improve the call_Seattle plot for better visualization and explaination

--- code/plot_calls.py
+++ code/plot_calls.py
@@ -2,6 +2,7 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from pathlib import Path
+import matplotlib.patheffects as pe
 
 script_dir = Path(__file__).parent
 clean_call = script_dir / '../data/derived-data/call_clean.csv'
@@ -21,19 +22,51 @@ plot_df = neigh_gdf.merge(call_group,how='left',
                           right_on='neighborhood').fillna(0)
 
 
-fig, ax = plt.subplots(figsize=(8, 6))
+fig, ax = plt.subplots(figsize=(9, 9))
 
 plot_df.plot(
     column='n_calls',
     cmap='Reds',
-    linewidth=0.5,
-    edgecolor='gray',
+    scheme='quantiles',
+    k=5,
+    linewidth=0.4,
+    edgecolor='white',
     legend=True,
+    legend_kwds={
+        'title': "CARE Calls (Quantiles)",
+        'fmt': "{:.0f}"
+        },
     ax=ax
 )
 
-ax.set_title("Number of Calls by Neighborhood", fontsize=14)
+plot_df['label_point'] = plot_df.geometry.representative_point()
+top_n = plot_df.nlargest(5, 'n_calls')
+
+for _, row in top_n.iterrows():
+    ax.text(
+        row['label_point'].x,
+        row['label_point'].y,
+        f"{row['L_HOOD']}\n{int(row['n_calls'])}",
+        ha='center',
+        va='center',
+        fontsize=6,
+        color='white',
+        weight='bold',
+        path_effects=[
+            pe.Stroke(linewidth=1, foreground='grey'),
+            pe.Normal()]
+    )
+
+ax.set_title("Number of CARE Calls by Large Neighborhood in Seattle", fontsize=14)
 ax.axis('off')
+
+plt.savefig(
+    script_dir / '../data/derived-data/calls_Seattle_enhanced.png',
+    dpi=300,
+    bbox_inches='tight'
+)
+
+plt.show()
 
 plt.savefig(
     script_dir / '../data/derived-data/calls_Seattle.png',
